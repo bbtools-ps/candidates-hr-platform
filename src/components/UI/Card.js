e84ce1d512696f1579ec7f872661da92ec5215ac
bogdanovicb <b.bogdanovic@case-3d.com>
2022-03-22 23:08:06 +0100

    change display type if window < 625px

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,12 +1,27 @@
+import { useState, useEffect } from "react";
 import styles from "./Card.module.css";
 
 const Card = ({ className, display = "flex", children }) => {
-  // change display type if window size for smaller screens
-  const windowWidth = window.innerWidth;
+  // change display type if window is smaller than 625px
+  const [displayStyle, setDisplayStyle] = useState(
+    window.innerWidth < 625 ? "grid" : display
+  );
+
+  useEffect(() => {
+    const updateLayout = () => {
+      window.innerWidth < 625
+        ? setDisplayStyle("grid")
+        : setDisplayStyle(display);
+    };
+    window.addEventListener("resize", updateLayout);
+    updateLayout();
+    return () => window.removeEventListener("resize", updateLayout);
+  }, [display]);
+
   return (
     <div
       className={`${className ? styles.card + " " + className : styles.card}`}
-      style={windowWidth <= 625 ? { display: "grid" } : { display: display }}
+      style={{ display: displayStyle }}
     >
       {children}
     </div>
