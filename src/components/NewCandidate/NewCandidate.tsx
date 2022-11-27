1ad221822c078ca00de862771f45e0828a2f6e41
Bogdan Bogdanovic <bogdi.mail@gmail.com>
2022-11-27 21:11:31 +0100

    Styling fixes

--- src/components/NewCandidate/NewCandidate.tsx
+++ src/components/NewCandidate/NewCandidate.tsx
@@ -1,5 +1,6 @@
 import uuid from "react-uuid";
 import { Candidate } from "../../common/models/Candidate";
+import classes from "../../common/styles/CandidateWrapper.module.css";
 import CandidateForm from "../CandidateForm/CandidateForm";
 import Card from "../UI/Card/Card";
 
@@ -10,14 +11,16 @@ interface NewCandidateProps {
 
 const NewCandidate: React.FC<NewCandidateProps> = ({ onCancel, onSubmit }) => {
   return (
-    <Card display="grid" styles={{ minWidth: "400px" }}>
-      <h2>New candidate</h2>
-      <CandidateForm
-        onCancel={onCancel}
-        onSubmit={onSubmit}
-        newCandidateId={uuid()}
-      />
-    </Card>
+    <div className={classes["candidate-wrapper"]}>
+      <Card display="grid">
+        <h2>New candidate</h2>
+        <CandidateForm
+          onCancel={onCancel}
+          onSubmit={onSubmit}
+          newCandidateId={uuid()}
+        />
+      </Card>
+    </div>
   );
 };
 
