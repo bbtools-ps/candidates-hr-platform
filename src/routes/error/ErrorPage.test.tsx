b374e4e0829c5bbbdf1783078f42055f4e079a70
Bogdan Bogdanovic <67063384+bbtools-ps@users.noreply.github.com>
2023-11-16 22:08:11 +0100

    tests update

--- src/routes/error/ErrorPage.test.tsx
+++ src/routes/error/ErrorPage.test.tsx
@@ -3,10 +3,10 @@ import { BrowserRouter } from "react-router-dom";
 import ErrorPage from "./ErrorPage";
 
 describe("<PageNotFound/>", () => {
-  it('should render the heading with the text "Page not found" and the link with the text "Go back home"', () => {
+  it('should render the heading with the text "Error" and the link with the text "Go back home"', () => {
     render(<ErrorPage />, { wrapper: BrowserRouter });
 
-    expect(screen.getByRole("heading")).toHaveTextContent(/error occured/i);
+    expect(screen.getByRole("heading")).toHaveTextContent(/error/i);
     expect(screen.getByRole("link")).toHaveTextContent(/go back home/i);
   });
 });
--- src/routes/error/ErrorPage.tsx
+++ src/routes/error/ErrorPage.tsx
@@ -1,11 +1,9 @@
 import { faTriangleExclamation } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Link, isRouteErrorResponse, useRouteError } from "react-router-dom";
+import { Link } from "react-router-dom";
 import classes from "./ErrorPage.module.css";
 
 const ErrorPage = () => {
-  const error = useRouteError();
-
   return (
     <div className={classes.container}>
       <h1>
@@ -14,9 +12,6 @@ const ErrorPage = () => {
         </span>
         Error
       </h1>
-      {isRouteErrorResponse(error) && (
-        <p className={classes.details}>{`${error.status} - ${error.data}`}</p>
-      )}
       <Link className={classes.link} to="/">
         Go back home
       </Link>
