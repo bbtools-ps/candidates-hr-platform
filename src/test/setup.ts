f16f66c3cd06547d97f520fb6f6fa2a882f51cf3
Bogdan Bogdanovic <67063384+bbtools-ps@users.noreply.github.com>
2024-07-16 14:22:01 +0200

    updated testing library

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,9 +1,7 @@
-import matchers from "@testing-library/jest-dom/matchers";
+import "@testing-library/jest-dom/vitest";
 import { cleanup } from "@testing-library/react";
 import ResizeObserver from "resize-observer-polyfill";
-import { afterEach, expect, vi } from "vitest";
-
-expect.extend(matchers);
+import { afterEach, vi } from "vitest";
 
 afterEach(() => {
   cleanup();
