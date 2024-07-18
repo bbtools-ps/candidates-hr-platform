a232a86fa9f294e2ad2fc2f9703e0e55c07139d4
Bogdan Bogdanovic <67063384+bbtools-ps@users.noreply.github.com>
2024-07-18 11:02:48 +0200

    added Checkbox component

---
+++ src/components/UI/Checkbox/Checkbox.tsx
@@ -0,0 +1,24 @@
+import { convertToKebabCase } from "@/utils";
+import { type InputHTMLAttributes } from "react";
+
+interface IProps
+  extends Omit<
+    React.DetailedHTMLProps<
+      InputHTMLAttributes<HTMLInputElement>,
+      HTMLInputElement
+    >,
+    "type" | "ref"
+  > {
+  label?: string;
+}
+
+export default function Checkbox({ label, id: idProp, ...rest }: IProps) {
+  const id = idProp || convertToKebabCase(label);
+
+  return (
+    <div className="flex gap-3">
+      {label && <label htmlFor={id}>{label}</label>}
+      <input type="checkbox" id={id} {...rest} />
+    </div>
+  );
+}
