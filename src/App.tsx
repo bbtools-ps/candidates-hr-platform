3b086dfb3587b66915b08b82d79fd3631a84054c
Bogdan Bogdanovic <67063384+bbtools-ps@users.noreply.github.com>
2025-04-23 16:52:57 +0200

    update typescript, react-router, vite and vitest, fix router configuration in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,40 +5,30 @@ import HomePage from "./routes";
 import ErrorPage from "./routes/ErrorPage";
 import RootLayout from "./routes/RootLayout";
 
-const router = createHashRouter(
-  [
-    {
-      path: "/",
-      element: <RootLayout />,
-      errorElement: <ErrorPage />,
-      hydrateFallbackElement: <Fallback />,
-      children: [
-        {
-          path: "",
-          element: <HomePage />,
-          children: [
-            {
-              path: "new-candidate",
-              lazy: () => import("./routes/new-candidate"),
-            },
-            {
-              path: "edit-candidate",
-              lazy: () => import("./routes/edit-candidate"),
-            },
-          ],
-        },
-      ],
-    },
-  ],
+const router = createHashRouter([
   {
-    future: {
-      v7_relativeSplatPath: true,
-      v7_fetcherPersist: true,
-      v7_normalizeFormMethod: true,
-      v7_partialHydration: true,
-    },
-  }
-);
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    hydrateFallbackElement: <Fallback />,
+    children: [
+      {
+        path: "",
+        element: <HomePage />,
+        children: [
+          {
+            path: "new-candidate",
+            lazy: () => import("./routes/new-candidate"),
+          },
+          {
+            path: "edit-candidate",
+            lazy: () => import("./routes/edit-candidate"),
+          },
+        ],
+      },
+    ],
+  },
+]);
 
 export default function App() {
   return <RouterProvider router={router} />;
