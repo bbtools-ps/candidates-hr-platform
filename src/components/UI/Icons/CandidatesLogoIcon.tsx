3839aa310614d91879927559cd2a705cf8b8578d
Bogdan Bogdanovic <67063384+bbtools-ps@users.noreply.github.com>
2025-04-27 21:30:32 +0200

    add aria-hidden attribute to CandidatesLogoIcon and Logo component for improved accessibility

--- src/components/UI/Icons/CandidatesLogoIcon.tsx
+++ src/components/UI/Icons/CandidatesLogoIcon.tsx
@@ -6,6 +6,7 @@ export default function CandidatesLogoIcon(
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 293.71 202.57"
       {...props}
+      aria-hidden="true"
     >
       <circle cx="146.72" cy="56.97" r="29.19" />
       <path
