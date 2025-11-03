b5d6a3a7a51780083551e01ef9ba3ad19e8c848f
Bogdan Bogdanovic <67063384+bbtools-ps@users.noreply.github.com>
2025-11-04 00:00:57 +0100

    refactor CandidatesList component to manage loading state and candidate filtering internally

--- src/components/CandidatesList/CandidatesList.tsx
+++ src/components/CandidatesList/CandidatesList.tsx
@@ -1,28 +1,42 @@
-import type { Candidate } from "@/models";
+import { DUMMY_CANDIDATES } from "@/data/data";
+import { useCandidatesStore } from "@/store/candidates";
 import { motion } from "motion/react";
+import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
+import { useSearchParams } from "react-router";
 import VirtualList from "../VirtualList/VirtualList";
 import CandidateItem from "./CandidateItem";
 
-interface CandidatesListProps {
-  candidates: Candidate[];
-  isLoading: boolean;
-  onToggleFavorite: (candidateId: string) => void;
-  onRemoveCandidate: (candidateId: string) => void;
-}
+export default function CandidatesList() {
+  const {
+    filterCandidates,
+    filteredCandidates,
+    removeCandidate,
+    setCandidates,
+    toggleFavorite,
+  } = useCandidatesStore();
 
-export default function CandidatesList({
-  candidates,
-  isLoading,
-  onToggleFavorite,
-  onRemoveCandidate,
-}: CandidatesListProps) {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setCandidates(DUMMY_CANDIDATES);
+    setIsLoading(false);
+  }, [setCandidates]);
+
+  const [searchParams] = useSearchParams();
+  const isFavorite = searchParams.get("isFavorite") === "true";
+  const searchTerm = searchParams.get("q") || "";
+
+  useEffect(() => {
+    filterCandidates(searchTerm, isFavorite);
+  }, [filterCandidates, isFavorite, searchTerm]);
+
   const { t } = useTranslation();
 
   return (
     <section className="flex flex-1 flex-col">
       {isLoading && <p>{t("Loading_Label")}</p>}
-      {candidates.length === 0 && (
+      {filteredCandidates.length === 0 && (
         <motion.div
           initial={{ opacity: 0, y: -30 }}
           animate={{ opacity: 1, y: 0 }}
@@ -32,19 +46,19 @@ export default function CandidatesList({
           {t("NoCandidatesFound_Label")}
         </motion.div>
       )}
-      {candidates.length > 0 && (
+      {filteredCandidates.length > 0 && (
         <motion.div
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           className="flex-1"
         >
-          <VirtualList items={candidates} className="h-full">
+          <VirtualList items={filteredCandidates} className="h-full">
             {(item) => (
               <div className="flex w-full justify-center">
                 <CandidateItem
                   candidate={item}
-                  onRemoveCandidate={onRemoveCandidate}
-                  onToggleFavorite={onToggleFavorite}
+                  onRemoveCandidate={removeCandidate}
+                  onToggleFavorite={toggleFavorite}
                 />
               </div>
             )}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,41 +1,10 @@
-import { DUMMY_CANDIDATES } from "@/data/data";
-import { useCandidatesStore } from "@/store/candidates";
-import { useEffect, useState } from "react";
-import { Outlet, useSearchParams } from "react-router";
+import { Outlet } from "react-router";
 import CandidatesList from "../components/CandidatesList/CandidatesList";
 
 export default function HomePage() {
-  const {
-    filterCandidates,
-    filteredCandidates,
-    removeCandidate,
-    setCandidates,
-    toggleFavorite,
-  } = useCandidatesStore();
-
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    setCandidates(DUMMY_CANDIDATES);
-    setIsLoading(false);
-  }, [setCandidates]);
-
-  const [searchParams] = useSearchParams();
-  const isFavorite = searchParams.get("isFavorite") === "true";
-  const searchTerm = searchParams.get("q") || "";
-
-  useEffect(() => {
-    filterCandidates(searchTerm, isFavorite);
-  }, [filterCandidates, isFavorite, searchTerm]);
-
   return (
     <>
-      <CandidatesList
-        candidates={filteredCandidates}
-        onRemoveCandidate={removeCandidate}
-        onToggleFavorite={toggleFavorite}
-        isLoading={isLoading}
-      />
+      <CandidatesList />
       <Outlet />
     </>
   );
